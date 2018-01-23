75d51fa9e09df71f0b6c4e58844490bbea6ba309
Todd Berliner <todd@altrooz.com>
2018-01-23 11:07:36 -0800

    Turned on source maps for dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 const paths = {
   DIST: path.resolve(__dirname, "dist"),
   SRC: path.resolve(__dirname, "src"),
+  ART: path.resolve(__dirname, "art"),
 };
 
 // Webpack config
@@ -16,6 +18,7 @@ const config = {
   devServer: {
     contentBase: paths.SRC,
   },
+  devtool: "eval-source-map",
   module: {
     rules: [
       {
@@ -29,6 +32,21 @@ const config = {
     new HtmlWebpackPlugin({
       title: "WeightTracker: The App",
       template: path.join(paths.SRC, "index.html"),
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.join(paths.ART, "wticon.png"),
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
     }),
   ],
 };
