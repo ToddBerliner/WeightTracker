8cd8562f44c938cbb6d3b46262c87cd90618a32b
Todd Berliner <todd@altrooz.com>
2018-01-26 08:49:54 -0800

    Initial design cut and start of styling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import WeightTracker from "./weighttracker";
 
-const styles = {
-  appWrap: {
-    display: "flex",
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-  },
-};
-
 // Load saved state and instantiate app
 const AppWrap = (
-  <div style={styles.appWrap}>
+  <div className="appWrap">
     <WeightTracker />
   </div>
 );
